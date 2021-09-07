5eb1876b14be4446fbbb7444b6561aca084c9756
Ruchika Gosain <sharma.ruchika34@gmail.com>
2021-09-06 22:12:58 -0400

    Update the Deleting Component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Form from "components/Appointment/Form";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -12,8 +14,8 @@ export default function Appointment(props) {
     const EMPTY = "EMPTY";
     const SHOW = "SHOW";
     const CREATE = "CREATE";
-    
-
+    const CONFIRM = "CONFIRM";
+    const DELETING = "DELETING";
 
     const { mode, transition, back } = useVisualMode (
         props.interview ? SHOW : EMPTY
@@ -33,6 +35,17 @@ export default function Appointment(props) {
       console.log("hitting this")
       transition(SHOW) });
   }
+
+
+  const deleteAppointment = () => {
+    transition(DELETING);
+    Promise.resolve(props.cancelInterview(props.id))
+      .then(() => transition(EMPTY))
+      .catch(err => {
+        transition(CONFIRM)
+        console.log(err)
+      });
+  };
 
     // useEffect(() => {
     
@@ -54,9 +67,10 @@ export default function Appointment(props) {
           {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
           {mode === SHOW && (
             <Show
-              // id={props.id}
+              id={props.id}
               student={props.interview.student}
               interviewer={props.interview.interviewer}
+              onDelete={() => transition(CONFIRM)}
 
             />
           )}
@@ -70,6 +84,18 @@ export default function Appointment(props) {
          
             />
           )}
+            {mode === CONFIRM && (
+        <Confirm 
+          message="Are you sure you want to delete?"
+          onConfirm={deleteAppointment}
+          onCancel = {back}
+        />
+      )}
+      {mode === DELETING && (
+        <Status 
+          message="Deleting"
+        />
+      )}
 
        
     </article>
