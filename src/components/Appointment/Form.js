67c6397d8270d149c577fc9273fccb6a1833f552
Ruchika Gosain <sharma.ruchika34@gmail.com>
2021-09-13 13:29:52 -0400

    Change the classname danger to confirm and remove the console.log

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -27,7 +27,7 @@ export default function Form(props) {
       props.onSave(name, interviewer);
     }
 
-    console.log(props.interviewers)
+  
     return (
         <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
@@ -48,7 +48,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel}>Cancel</Button>
-      <Button danger onClick={validate}>Save</Button>
+      <Button confirm onClick={validate}>Save</Button>
     </section>
   </section>
 </main>
